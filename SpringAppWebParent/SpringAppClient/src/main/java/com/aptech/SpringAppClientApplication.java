1d8ebc1398a6450fcbbfab6beca46b5bc69fc271
qnguyen95 <quangtri.nguyen@dxc.com>
2022-06-30 15:32:32 +0700

    Add entity User & Role

--- SpringAppWebParent/SpringAppClient/src/main/java/com/aptech/SpringAppClientApplication.java
+++ SpringAppWebParent/SpringAppClient/src/main/java/com/aptech/SpringAppClientApplication.java
@@ -2,8 +2,10 @@ package com.aptech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.aptech.common.entity"})
 public class SpringAppClientApplication {
 
 	public static void main(String[] args) {
