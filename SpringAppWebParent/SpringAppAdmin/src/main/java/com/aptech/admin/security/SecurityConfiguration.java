86802e1027e8e297c345a7d883d97dab50d14909
qnguyen95 <quangtri.nguyen@dxc.com>
2022-07-03 22:24:50 +0700

    #Add Code Module User

---
+++ SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/security/SecurityConfiguration.java
@@ -0,0 +1,75 @@
+package com.aptech.admin.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new TimeZoneUserDetailsService();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		.antMatchers("/states/list_by_country/**").hasAnyAuthority("Admin", "Salesperson")
+		.antMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
+		.antMatchers("/categories/**", "/brands/**").hasAnyAuthority("Admin", "Editor")
+		
+		.antMatchers("/products/new", "/products/delete/**").hasAnyAuthority("Admin", "Editor")
+		
+		.antMatchers("/products/edit/**", "/products/save", "/products/check_unique")
+			.hasAnyAuthority("Admin", "Editor", "Salesperson")
+			
+		.antMatchers("/products", "/products/", "/products/detail/**", "/products/page/**")
+			.hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+			
+		.antMatchers("/products/**").hasAnyAuthority("Admin", "Editor")
+		
+		.antMatchers("/orders", "/orders/", "/orders/page/**", "/orders/detail/**").hasAnyAuthority("Admin", "Salesperson", "Shipper")
+		
+		.antMatchers("/products/detail/**", "/customers/detail/**").hasAnyAuthority("Admin", "Editor", "Salesperson", "Assistant")
+
+		.antMatchers("/customers/**", "/orders/**", "/get_shipping_cost", "/reports/**").hasAnyAuthority("Admin", "Salesperson")
+		
+		.antMatchers("/orders_shipper/update/**").hasAuthority("Shipper")
+		
+		.antMatchers("/reviews/**").hasAnyAuthority("Admin", "Assistant")
+		
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()			
+			.loginPage("/login")
+			.usernameParameter("email")
+			.permitAll()
+		.and().logout().permitAll()
+		.and()
+			.rememberMe()
+				.key("AbcDefgHijKlmnOpqrs_1234567890")
+				.tokenValiditySeconds(7 * 24 * 60 * 60);
+				;
+		http.headers().frameOptions().sameOrigin();
+
+		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+
+}
---
+++ SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.aptech.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
