3eef33e44f14449337a5340c65db13ebdf126bbb
qnguyen95 <quangtri.nguyen@dxc.com>
2022-07-04 17:58:06 +0700

    Check Uniqueness of User Email

--- SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/controller/AccountController.java
+++ SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/controller/AccountController.java
@@ -21,45 +21,5 @@ import com.aptech.common.entity.User;
 @Controller
 public class AccountController {
 
-	@Autowired
-	private UserService service;
 	
-	@GetMapping("/account")
-	public String viewDetails(@AuthenticationPrincipal TimeZoneUserDetails loggedUser,
-			Model model) {
-		String email = loggedUser.getUsername();
-		User user = service.getByEmail(email);
-		model.addAttribute("user", user);
-		
-		return "users/account_form";
-		
-	}
-	
-	@PostMapping("/account/update")
-	public String saveDetails(User user, RedirectAttributes redirectAttributes,
-			@AuthenticationPrincipal TimeZoneUserDetails loggedUser,
-			@RequestParam("image") MultipartFile multipartFile) throws IOException {
-		
-		if (!multipartFile.isEmpty()) {
-			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-			user.setPhotos(fileName);
-			User savedUser = service.updateAccount(user);
-			
-			String uploadDir = "user-photos/" + savedUser.getId();
-			
-			FileUploadUtil.cleanDir(uploadDir);
-			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-			
-		} else {
-			if (user.getPhotos().isEmpty()) user.setPhotos(null);
-			service.updateAccount(user);
-		}
-		
-		loggedUser.setFirstName(user.getFirstName());
-		loggedUser.setLastName(user.getLastName());
-		
-		redirectAttributes.addFlashAttribute("message", "Your account details have been updated.");
-		
-		return "redirect:/account";
-	}	
 }
