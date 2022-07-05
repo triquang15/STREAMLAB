1edf04de2dad07d3c885e0c1e5bc104fc16dd0c1
qnguyen95 <quangtri.nguyen@dxc.com>
2022-07-05 10:59:25 +0700

    #Code Update User Function

--- SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/controller/UserController.java
+++ SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/controller/UserController.java
@@ -62,4 +62,23 @@ public class UserController {
 		
 		return "redirect:/users";
 	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			User user = service.get(id);
+			List<Role> listRoles = service.listRoles();
+			
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			model.addAttribute("listRoles", listRoles);
+			
+			return "users/user_form";
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+			return "redirect:/users";
+		}
+	}
 }
