1a02076a2e9cfedb99b1ad8461c135062839fe5e
qnguyen95 <quangtri.nguyen@dxc.com>
2022-07-05 12:28:17 +0700

    #Code User Update Enable Status

--- SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/UserService.java
+++ SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/user/UserService.java
@@ -19,60 +19,62 @@ import com.aptech.common.entity.User;
 public class UserService {
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private RoleRepository roleRepo;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
 	}
-	
+
 	public List<Role> listRoles() {
 		return (List<Role>) roleRepo.findAll();
 	}
 
 	public void save(User user) {
 		boolean isUpdatingUser = (user.getId() != null);
-		
+
 		if (isUpdatingUser) {
 			User existingUser = userRepo.findById(user.getId()).get();
-			
+
 			if (user.getPassword().isEmpty()) {
 				user.setPassword(existingUser.getPassword());
 			} else {
 				encodePassword(user);
 			}
-			
-		} else {		
+
+		} else {
 			encodePassword(user);
 		}
-		
+
 		userRepo.save(user);
 	}
-	
+
 	private void encodePassword(User user) {
 		String encodedPassword = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodedPassword);
 	}
-	
+
 	public boolean isEmailUnique(Integer id, String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		
-		if (userByEmail == null) return true;
-		
+
+		if (userByEmail == null)
+			return true;
+
 		boolean isCreatingNew = (id == null);
-		
+
 		if (isCreatingNew) {
-			if (userByEmail != null) return false;
+			if (userByEmail != null)
+				return false;
 		} else {
 			if (userByEmail.getId() != id) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 
@@ -83,13 +85,17 @@ public class UserService {
 			throw new UserNotFoundException("Could not find any user with ID " + id);
 		}
 	}
-	
+
 	public void delete(Integer id) throws UserNotFoundException {
 		Long countById = userRepo.countById(id);
 		if (countById == null || countById == 0) {
 			throw new UserNotFoundException("Could not find any user with ID " + id);
 		}
-		
+
 		userRepo.deleteById(id);
+	}
+
+	public void updateUserEnabledStatus(Integer id, boolean enabled) {
+		userRepo.updateEnabledStatus(id, enabled);
 	}
 }
