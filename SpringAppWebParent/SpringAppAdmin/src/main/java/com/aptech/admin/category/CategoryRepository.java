a062aa09442a2d2afa4ec95ddc6973b564122a27
qnguyen95 <quangtri.nguyen@dxc.com>
2022-07-07 10:31:41 +0700

    #Code Module Category

--- SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/category/CategoryRepository.java
+++ SpringAppWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/category/CategoryRepository.java
@@ -15,7 +15,7 @@ import com.aptech.common.entity.Category;
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
 	
 	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
-	public List<Category> findRootCategories();
+	public List<Category> findRootCategories(Sort sort);
 	
 	@Query("SELECT c FROM Category c WHERE c.name LIKE %?1%")
 	public Page<Category> search(String keyword, Pageable pageable);
